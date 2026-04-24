29fcd23d0082ae1b27d3bbb249abe04bd0f9ff2b
youssefhemdane <youssefhemdaneazzabi@gmail.com>
2026-04-24 11:09:25 +0100

    Initialize Flask app to serve dashboard.html

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+    return send_file("dashboard.html")
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=8000)
